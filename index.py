9a753bef7677435b8f76fa81e1d8fbc1e08d88f5
Karun Kris <karunk563@gmail.com>
2020-12-14 00:14:27 +0530

    Create index.py
    Add the python script for web scraping

---
+++ index.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+
+#connecting to the website and creating the beautiful soup object
+source = requests.get("https://www.dictionary.com/e/word-of-the-day/", timeout=5).text
+soup=BeautifulSoup(source,"lxml")
+
+print("The Word of the Day\n")
+
+#printing the word of the day
+word = soup.find("div", class_="otd-item-headword__word")
+wod= word.h1.text
+print(wod.upper())
+
+#printing the type of the word
+wordType = soup.find("span", class_="luna-pos")
+print(wordType.text)
+
+#printing the definition of the word
+d = soup.find("div", class_="otd-item-headword__pos")
+definition = d.find_all("p")
+print("\nDefinition of "+wod+": "+definition[1].text)
+
+#printing examples of the word
+example = soup.find("div", class_="wotd-item-example__content")
+print("\nExample of "+wod+" in a sentence: "+"'"+example.p.text+"'")
